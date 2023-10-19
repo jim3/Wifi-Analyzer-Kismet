826bf525be38d4dd5ea23b61b7a8de63fa0df9ef
jim3 <jimmy3.xyz@gmail.com>
2023-10-19 18:51:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+// ((((((((( Wifi Analyzer ))))))))) //
+
+const axios = require("axios");
+require("dotenv").config();
+const util = require("util");
+
+// -------------------------------------------------------- //
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const server = process.env.SERVER;
+const port = process.env.PORT;
+const url = `http://${username}:${password}@${server}:${port}`;
+const check_session = "/session/check_session";
+const all_sources = `${url}/datasource/all_sources.json`;
+
+// -------------------------------------------------------- //
+
+// Function to check session validity
+const checkSession = async () => {
+    try {
+        const response = await axios.get(`${url}${check_session}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Function to fetch access points data and related clients
+const fetchAccessPoints = async () => {
+    try {
+        const response = await axios.get(
+            `${url}/devices/views/phydot11_accesspoints/devices.json`
+        );
+        const data = response.data;
+        const accessPoints = Object.keys(data).map((key) => data[key]);
+
+        // Function to fetch related clients for a given device
+        const fetchRelatedClients = async (deviceKey) => {
+            try {
+                const response = await axios.get(
+                    `${url}/phy/phy80211/related-to/${deviceKey}/devices.json`
+                );
+                const clientsData = response.data;
+                console.log("Clients for Device Key:", deviceKey);
+                console.log(util.inspect(clientsData, { depth: 2, compact: false })); // for debugging
+            } catch (error) {
+                console.error("Error fetching related clients:", error);
+            }
+        };
+
+        accessPoints.forEach((accessPoint) => {
+            const deviceKey = accessPoint["kismet.device.base.key"] || {};
+            fetchRelatedClients(deviceKey); // fetch related clients for this device
+        });
+    } catch (error) {
+        console.error("Error fetching access points:", error);
+    }
+};
+
+checkSession();
+fetchAccessPoints();
